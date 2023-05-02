7c6ca6b097f543c85cb3fc9636713e4db9c4c391
Murat Dzhulkuttiev <dzhulk@gmail.com>
2023-05-02 14:33:31 +0700

    update

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,21 @@
 extern crate rustyline;
 
-// use rustyline::error::ReadlineError;
-// use rustyline::DefaultEditor;
-// use std::collections::LinkedList;
+use std::collections::HashMap;
 
+use std::cmp::{Eq, PartialEq};
 use std::fs;
-use std::cmp::{PartialEq, Eq};
 
 const SKIP_SYMS: [char; 4] = ['\n', '\r', ' ', '\t'];
 const OPS: [char; 7] = ['+', '-', '=', '*', '/', '>', '<'];
 
 #[derive(Debug, PartialEq, Eq)]
 enum Token {
-    LPAR,
-    RPAR,
-    NUM(String),
-    SYM(String),
-    STR(String),
-    OP(String),
+    LPar,
+    RPar,
+    Num(String),
+    Sym(String),
+    Str(String),
+    Op(String),
 }
 
 #[derive(Debug)]
@@ -28,9 +26,9 @@ struct Lexer<'a> {
 impl<'a> Lexer<'a> {
     fn peek(&self) -> Option<char> {
         if self.input.is_empty() {
-            return None
+            return None;
         } else {
-            return Some(self.input[0])
+            return Some(self.input[0]);
         }
     }
 
@@ -40,7 +38,8 @@ impl<'a> Lexer<'a> {
         return chr;
     }
 
-    fn chop_while<F>(&mut self, f: F) -> String where F: Fn(char)->bool {
+    fn chop_while(&mut self, f: impl Fn(char) -> bool) -> String
+    {
         let mut result: Vec<char> = Vec::new();
         while let Some(nxt) = self.peek() {
             if f(nxt) {
@@ -53,40 +52,40 @@ impl<'a> Lexer<'a> {
     }
 
     fn parse(&mut self) -> Vec<Token> {
-        let mut tokens : Vec<Token> = Vec::new();
+        let mut tokens: Vec<Token> = Vec::new();
         while let Some(_) = self.peek() {
             match self.peek() {
                 Some('(') => {
                     self.next();
-                    tokens.push(Token::LPAR);
-                },
+                    tokens.push(Token::LPar);
+                }
                 Some(')') => {
                     self.next();
-                    tokens.push(Token::RPAR);
-                },
+                    tokens.push(Token::RPar);
+                }
                 Some(s) if s.is_alphabetic() => {
                     let token = self.chop_while(|c| c.is_alphanumeric());
-                    tokens.push(Token::SYM(token))
+                    tokens.push(Token::Sym(token))
                 }
                 Some(s) if s.is_numeric() => {
                     let token = self.chop_while(|c| c.is_numeric());
-                    tokens.push(Token::NUM(token));
+                    tokens.push(Token::Num(token));
                 }
                 Some(s) if SKIP_SYMS.contains(&s) => {
                     self.next();
                 }
                 Some(s) if OPS.contains(&s) => {
-                    tokens.push(Token::OP(self.next().to_string()));
-                },
+                    tokens.push(Token::Op(self.next().to_string()));
+                }
                 Some('"') => {
                     self.next();
-                    tokens.push(Token::STR(self.chop_while(|ch| ch != '"')));
+                    tokens.push(Token::Str(self.chop_while(|ch| ch != '"')));
                     self.next();
-                },
+                }
                 any => {
                     println!("Unknown {any:?}");
                     todo!();
-                },
+                }
             };
         }
         return tokens;
@@ -103,22 +102,40 @@ enum SExp {
     Nil,
 }
 
-
 impl SExp {
     fn new_list(value: SExp) -> SExp {
-        return SExp::Cons { car: Box::new(value), cdr: Box::new(SExp::Nil) };
+        return SExp::Cons {
+            car: Box::new(value),
+            cdr: Box::new(SExp::Nil),
+        };
+    }
+
+    fn is_id(&self, val: &str) -> bool {
+        match self {
+            Self::Id(id) => *id == String::from(val),
+            _ => false,
+        }
     }
 
     fn append_to_list(self, other: SExp) -> SExp {
         match self {
             Self::Cons { car, cdr } => {
                 if *cdr == SExp::Nil {
-                    let new_list = SExp::Cons { car: Box::new(other), cdr: Box::new(SExp::Nil) };
-                    Self::Cons { car: car, cdr: Box::new(new_list) }
+                    let new_list = Self::Cons {
+                        car: Box::new(other),
+                        cdr: Box::new(SExp::Nil),
+                    };
+                    Self::Cons {
+                        car: car,
+                        cdr: Box::new(new_list),
+                    }
                 } else {
-                    Self::Cons { car: car, cdr: Box::new(cdr.append_to_list(other)) }
+                    Self::Cons {
+                        car: car,
+                        cdr: Box::new(cdr.append_to_list(other)),
+                    }
                 }
-            },
+            }
             _ => {
                 eprintln!("ERROR: append to not list type");
                 todo!();
@@ -129,78 +146,120 @@ impl SExp {
 
 fn parse_sexp(token: &Token) -> SExp {
     match token {
-        Token::SYM(str) => {
+        Token::Sym(str) => {
             if str == "quote" {
                 SExp::Quote
             } else {
                 SExp::Id(String::from(str))
             }
-        },
-        Token::STR(str) => SExp::Sym(String::from(str)),
-        Token::OP(str)  => SExp::Id(String::from(str)),
-        Token::NUM(str) => SExp::Num(String::from(str).parse::<i32>().unwrap()),
+        }
+        Token::Str(str) => SExp::Sym(String::from(str)),
+        Token::Op(str) => SExp::Id(String::from(str)),
+        Token::Num(str) => SExp::Num(String::from(str).parse::<i32>().unwrap()),
         any => {
             eprintln!("ERROR: Unknown token: {any:?}");
             todo!();
-        },
+        }
     }
 }
 
-fn parse_list(input: &[Token]) -> SExp {
-    let mut stack: Vec<SExp> = Vec::new();
-    let mut tokens = input;
+#[derive(Debug)]
+struct Parser {
+    tokens: Vec<Token>,
+    sexprs: Vec<SExp>,
+}
 
-    while !tokens.is_empty() {
-        match tokens[0] {
-            Token::LPAR => {
-                let curr_sexp = SExp::new_list(parse_sexp(&tokens[1]));
-                stack.push(curr_sexp);
-                tokens = &tokens[2..];
-            },
-            Token::RPAR => {
-                println!("Stack len: {len}", len = stack.len());
-                if stack.len() == 1 { // because we skiped first LPAR
-                    println!("Left tokens: {tokens:?}"); // should be RPAR
-                    break;
-                } else {
-                    let curr_sexp = stack.pop().unwrap();
-                    let parent = stack.pop().unwrap();
-                    let new_list = parent.append_to_list(curr_sexp);
-                    stack.push(new_list);
-                    tokens = &tokens[1..];
-                }
-            },
-            Token::SYM(_) | Token::STR(_) | Token::OP(_) | Token::NUM(_) => {
-                let curr_sexp = stack.pop().unwrap();
-                let new_list = curr_sexp.append_to_list(parse_sexp(&tokens[0]));
-                stack.push(new_list);
-                tokens = &tokens[1..];
-            },
-        }
+impl Parser {
+    fn new(tokens: Vec<Token>) -> Self {
+        return Parser {
+            tokens: tokens,
+            sexprs: Vec::new(),
+        };
     }
 
-    return stack.pop().unwrap();
+    fn drop_tokens(&mut self, n: usize) {
+        self.tokens = self.tokens.drain(n..).collect();
+    }
+
+    fn parse_tokens(&mut self) {
+        let mut stack: Vec<SExp> = Vec::new();
+
+        while !self.tokens.is_empty() {
+            match self.tokens[0] {
+                Token::LPar => {
+                    stack.push(SExp::new_list(parse_sexp(&self.tokens[1])));
+                    self.drop_tokens(2);
+                }
+                Token::RPar => {
+                    if stack.len() == 1 {
+                        self.drop_tokens(1);
+                        self.sexprs.push(stack.pop().unwrap());
+                    } else {
+                        let curr_sexp = stack.pop().unwrap();
+                        let parent = stack.pop().unwrap();
+                        let new_list = parent.append_to_list(curr_sexp);
+                        stack.push(new_list);
+                        self.drop_tokens(1);
+                    }
+                }
+                Token::Sym(_) | Token::Str(_) | Token::Op(_) | Token::Num(_) => {
+                    let new_list = stack
+                        .pop()
+                        .unwrap()
+                        .append_to_list(parse_sexp(&self.tokens[0]));
+                    stack.push(new_list);
+                    self.drop_tokens(1);
+                }
+            }
+        }
+    }
 }
 
 struct EvalEnvironment {
-
+    met_table: HashMap<SExp, SExp>,
+    var_table: HashMap<SExp, SExp>,
 }
 
+impl EvalEnvironment {
+    fn new() -> Self {
+        return EvalEnvironment {
+            met_table: HashMap::new(),
+            var_table: HashMap::new(),
+        };
+    }
+}
+
+// fn eval_expr(expr: SExp, env: &mut EvalEnvironment) -> SExp {
+//     match expr {
+//         SExp::Cons { car, cdr } if car.is_id("define") => {}
+//         _ => {}
+//     }
+// }
+
 fn main() {
-    let args= std::env::args();
+    let args = std::env::args();
 
     let arguments = args.collect::<Vec<_>>();
 
-    let file_path  = &arguments[1];
+    let file_path = &arguments[1];
     println!("Reading {file_path}");
     let content = fs::read_to_string(file_path).expect("Cant read file content");
     let chars = content.chars().into_iter().collect::<Vec<_>>();
     let mut lexer = Lexer { input: &chars };
     let tokens = lexer.parse();
     println!("Token: #{tokens:?}");
-    let sexp = parse_list(tokens.as_slice());
-    println!("SExpr: {sexp:?}");
+    let mut parser = Parser::new(tokens);
+    parser.parse_tokens();
+    println!("Parser: {parser:?}");
 
+
+
+    // let s1 = SExp::Id("some 32".to_string());
+    // let s2 = SExp::Id("some 32".to_string());
+
+    // if s1 == s2 {
+    //     println!("EQUAL!");
+    // }
 }
 
 /*
